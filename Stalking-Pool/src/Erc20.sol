c74b32ec3a0045dbb8a690316221b50bf78d15c7
FILIBUS YILRIT DIMKA <110455012+dimka90@users.noreply.github.com>
2024-11-28 06:31:54 +0100

    Update Erc20.sol

--- Stalking-Pool/src/Erc20.sol
+++ Stalking-Pool/src/Erc20.sol
@@ -99,18 +99,3 @@ function getSender() view external returns(address)
         balances[owner] -= amount;
     }
 }
-
-// forge create --rpc-url https://rpc.sepolia-api.lisk.com \
-// --etherscan-api-key GTT7DH386XJ2JDK8RAU64UPYBT1D4GGEQW\
-// --verify \
-// --verifier blockscout \
-// --verifier-url https://sepolia-blockscout.lisk.com/api \
-// --private-key 3c52739c9a662defb57de6b8aa2d742a0e42f95ef4c7a1064428e39f7fc61909\
-// src/Staking_pool.sol:Staking
-
-// forge verify-contract 0xD5706cE7B5304e3F9Fe56C72b4892Be90B445686 \
-// ./src/Staking_pool.sol:Staking\
-// --chain 4202 \
-// --watch \
-// --verifier blockscout \
-// --verifier-url https://sepolia-blockscout.lisk.com/api
